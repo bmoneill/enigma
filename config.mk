1a9a1b29f0939c7781cb54c2bf61ee5f54458318
Ben O'Neill <ben@oneill.sh>
2025-07-16 09:55:24 -0400

    Remove optimizations

--- config.mk
+++ config.mk
@@ -5,7 +5,7 @@ PREFIX   = /usr/local
 CC       = cc
 LD       = $(CC)
 
-CFLAGS   = -O2 -Werror -std=c99 -pedantic -g
+CFLAGS   = -Werror -std=c99 -pedantic -g
 CFLAGS   += -DVERBOSE
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 \
            -D_FILE_OFFSET_BITS=64 -DVERSION="$(VERSION)"
