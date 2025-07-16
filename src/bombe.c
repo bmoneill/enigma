9518a6b645f5277521caab552a28fac35f141ea1
Ben O'Neill <ben@oneill.sh>
2025-07-16 14:32:42 -0400

    Get bombe working

--- src/bombe.c
+++ src/bombe.c
@@ -11,8 +11,8 @@
 #define ALPHA_SIZE 26
 
 static void bombe_print_partial_configuration(enigma_t *enigma);
-static void bombe_process_chunk(bombe_t *, enigma_t *, const char *, int);
-static void bombe_process_single(bombe_t *, enigma_t *, const char *, int);
+static void bombe_process_chunk(bombe_t *, enigma_t *, const char *, int, char *);
+static void bombe_process_single(bombe_t *, enigma_t *, const char *, int, char *, const char *);
 
 /**
  * @brief Initializes the Bombe structure with cribs and Enigma configuration.
@@ -39,7 +39,7 @@ void bombe_init(bombe_t *bombe, char **cribStrings, int *cribIndices, int numCri
  * @param threadCount Number of threads to use for processing
  */
 void bombe_run(bombe_t *bombe, const char *ciphertext) {
-    int rotor_count = 8;
+    int rotor_count = 3;
     int reflector_count = 3;
     int plugboard_count = 0;
     int ciphertextLength = strlen(ciphertext);
@@ -49,6 +49,21 @@ void bombe_run(bombe_t *bombe, const char *ciphertext) {
     for (int i = 0; i < bombe->numCribs; i++) {
         printf(" * %d %s\n", bombe->crib[i].index, bombe->crib[i].s);
     }
+
+    enigma_t enigma;
+    enigma.rotors = malloc(rotor_count * sizeof(rotor_t));
+    enigma.reflector = malloc(sizeof(reflector_t));
+    enigma.plugboard = NULL;
+    enigma.rotor_count = 3;
+    enigma.rotor_flag = 0;
+    memcpy(&enigma.rotors[2], &rotor_I, sizeof(rotor_t));
+    memcpy(&enigma.rotors[1], &rotor_II, sizeof(rotor_t));
+    memcpy(&enigma.rotors[0], &rotor_III, sizeof(rotor_t));
+    memcpy(enigma.reflector, &UKW_B, sizeof(reflector_t));
+
+    char *plaintext = malloc(ciphertextLength + 1);
+
+    bombe_process_chunk(bombe, &enigma, ciphertext, ciphertextLength, plaintext);
 }
 
 /**
@@ -59,11 +74,13 @@ void bombe_run(bombe_t *bombe, const char *ciphertext) {
  *
  * This loop runs 26^3=17576 times.
  */
-static void bombe_process_chunk(bombe_t *bombe, enigma_t *enigma, const char *ciphertext, int ciphertextLength) {
+static void bombe_process_chunk(bombe_t *bombe, enigma_t *enigma, const char *ciphertext, int ciphertextLength, char *plaintext) {
     // Placeholder for processing each chunk of ciphertext
-    printf("Processing chunk with rotors (%s, %s, %s), reflector %s, plugboard %s\n",
+    printf("Processing chunk with rotors (%s, %s, %s), reflector %s\n",
            enigma->rotors[0].name, enigma->rotors[1].name, enigma->rotors[2].name,
-           enigma->reflector->name, enigma->plugboard);
+           enigma->reflector->name);
+
+    char configString[256];
 
     for (int i = 0; i < ALPHA_SIZE; i++) {
         for (int j = 0; j < ALPHA_SIZE; j++) {
@@ -72,9 +89,14 @@ static void bombe_process_chunk(bombe_t *bombe, enigma_t *enigma, const char *ci
                 enigma->rotors[2].idx = i;
                 enigma->rotors[1].idx = j;
                 enigma->rotors[0].idx = k;
+                sprintf(configString, "Rotors: %s (%c), %s (%c), %s (%c), Reflector: %s",
+                        enigma->rotors[0].name, enigma->rotors[0].idx + 'A',
+                        enigma->rotors[1].name, enigma->rotors[1].idx + 'A',
+                        enigma->rotors[2].name, enigma->rotors[2].idx + 'A',
+                        enigma->reflector->name);
 
                 // Run Bombe algorithm with the current configuration
-                bombe_process_single(bombe, enigma, ciphertext, ciphertextLength);
+                bombe_process_single(bombe, enigma, ciphertext, ciphertextLength, plaintext, configString);
             }
         }
     }
@@ -83,7 +105,7 @@ static void bombe_process_chunk(bombe_t *bombe, enigma_t *enigma, const char *ci
 /**
  * @brief Check the given enigma configuration against the ciphertext.
  */
-static void bombe_process_single(bombe_t *bombe, enigma_t *enigma, const char *ciphertext, int ciphertextLength) {
+static void bombe_process_single(bombe_t *bombe, enigma_t *enigma, const char *ciphertext, int ciphertextLength, char *plaintext, const char *configString) {
     int matching = -1;
     int matchedLength = 0;
 
@@ -93,17 +115,17 @@ static void bombe_process_single(bombe_t *bombe, enigma_t *enigma, const char *c
         }
 
         char decrypted = encode(enigma, ciphertext[i]);
+        plaintext[i] = decrypted;
 
         if (matching == -1) {
             for (int j = 0; j < bombe->numCribs; j++) {
-                if (bombe->crib[j].index == i) {
+                if (bombe->crib[j].index == i && bombe->crib[j].s[0] == decrypted) {
                     matching = j;
+                    matchedLength++;
                     break;
                 }
             }
-        }
-
-        if (matching != -1) {
+        } else {
             if (decrypted == bombe->crib[matching].s[matchedLength]) {
                 matchedLength++;
                 if (matchedLength == bombe->crib[matching].length) {
@@ -116,32 +138,5 @@ static void bombe_process_single(bombe_t *bombe, enigma_t *enigma, const char *c
         }
     }
 
-    printf("Potential configuration:\n * Rotors: %s (%d) %s (%d) %s (%d)\n * Reflector: %s\n * Plugboard: %s\n",
-            enigma->rotors[0].name, enigma->rotors[0].idx,
-            enigma->rotors[1].name, enigma->rotors[1].idx,
-            enigma->rotors[2].name, enigma->rotors[2].idx,
-            enigma->reflector->name, enigma->plugboard ? enigma->plugboard : "None");
-}
-
-static void bombe_print_partial_configuration(enigma_t *enigma) {
-    printf("Using partial Enigma configuration:\n");
-    if (enigma->rotors) {
-        printf("Rotors: ");
-        for (int i = 0; i < enigma->rotor_count; i++) {
-            if (enigma->rotors[i].name) {
-                printf("%s ", enigma->rotors[i].name);
-            } else {
-                printf("X ");
-            }
-        }
-        printf("\n");
-    }
-
-    if (enigma->plugboard) {
-        printf("Plugboard: %s\n", enigma->plugboard);
-    }
-
-    if (enigma->reflector) {
-        printf("Reflector: %s\n", enigma->reflector->name);
-    }
+    printf("%s: %s\n", configString, plaintext);
 }
