da9e05790cb5505209970c52078025c9c1773926
Ben O'Neill <ben@oneill.sh>
2025-07-17 13:57:37 -0400

    Add thread count option

--- src/bombe_main.c
+++ src/bombe_main.c
@@ -9,38 +9,41 @@ static void print_usage(const char *argv0);
 
 int main(int argc, char *argv[]) {
     char *crib = NULL;
-    int idx = 0;
+    int idx = -1;
     char *ciphertext = NULL;
+    int threadCount = 1;
 
     // Parse command line options
     // Again need better names here
     int opt;
-    while ((opt = getopt(argc, argv, "c:C:i:")) != -1) {
+    while ((opt = getopt(argc, argv, "c:C:i:t:")) != -1) {
         switch (opt) {
         case 'c': crib = optarg; break;
         case 'C': ciphertext = optarg; break;
         case 'i': idx = atoi(optarg); break;
+        case 't': threadCount = atoi(optarg); break;
         default: print_usage(argv[0]);
         }
     }
 
-    if (!crib || !ciphertext) {
+    if (!crib || !ciphertext || idx < 0) {
         fprintf(stderr, "Error: Crib, index, and ciphertext must be provided.\n\n");
         print_usage(argv[0]);
     }
 
     bombe_t bombe;
     bombe_init(&bombe, (char *[]){crib}, (int []){idx}, 1);
-    bombe_run(&bombe, ciphertext, 6);
+    bombe_run(&bombe, ciphertext, threadCount);
 
     return EXIT_SUCCESS;
 }
 
 static void print_usage(const char *argv0) {
-    fprintf(stderr, "Usage: %s -c crib -i index -C ciphertext\n", argv0);
+    fprintf(stderr, "Usage: %s [-t thread_count] -c crib -i index -C ciphertext\n", argv0);
     fprintf(stderr, "Options:\n");
     fprintf(stderr, "  -c crib       Set the crib string to use\n");
     fprintf(stderr, "  -C ciphertext Set the ciphertext to analyze\n");
     fprintf(stderr, "  -i index      Set the index where the crib string should be in the ciphertext\n");
+    fprintf(stderr, "  -t threadCount Set the number of threads to use\n");
     exit(EXIT_FAILURE);
 }
