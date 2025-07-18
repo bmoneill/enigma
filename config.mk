d9f294a131dbb08d53218c90494140e16e2b222d
Ben O'Neill <ben@oneill.sh>
2025-07-18 13:03:37 -0400

    config.mk: Don't include debugging symbols by default

--- config.mk
+++ config.mk
@@ -5,7 +5,7 @@ PREFIX   = /usr/local
 CC       = cc
 LD       = $(CC)
 
-CFLAGS        = -Werror -std=c99 -pedantic -g
+CFLAGS        = -Werror -std=c99 -pedantic #-g -pg
 #ENIGMA_CFLAGS = -DVERBOSE
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 \
            -D_FILE_OFFSET_BITS=64 -DVERSION="$(VERSION)"
