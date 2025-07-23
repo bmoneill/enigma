027802934df36ceefd056b1bdfe5b476ebe12bc2
Ben O'Neill <ben@oneill.sh>
2025-07-23 16:14:46 -0400

    config.mk: Format

--- config.mk
+++ config.mk
@@ -1,12 +1,12 @@
-VERSION  = 0.1
+VERSION       = 0.1
 
-PREFIX   = /usr/local
+PREFIX        = /usr/local
 
-CC       = gcc
-LD       = $(CC)
+CC            = gcc
+LD            = $(CC)
 
 CFLAGS        = -Werror -std=c99 -pedantic -Ofast
 # CFLAGS      += -g -pg
 # CFLAGS      += -DVERBOSE
-CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 \
-           -D_FILE_OFFSET_BITS=64 -DVERSION="$(VERSION)"
+CPPFLAGS      = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 \
+                -D_FILE_OFFSET_BITS=64 -DVERSION="$(VERSION)"
