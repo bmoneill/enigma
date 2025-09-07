5b5b7164134cad3b1eb718280b54db90899b388c
Ben O'Neill <ben@oneill.sh>
2025-09-06 20:18:27 -0400

    Modify ngram script

--- tools/ngram.sh
+++ tools/ngram.sh
@@ -1,14 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 # ngram generator
-# usage: ngram.sh n < file
+# usage: ngram.sh n file
 
 n=$1
-while read line; do
-echo $line | tr ' ' '\n' | tr -cd '[:alpha:] \n' | tr '[:upper:]' '[:lower:]' | awk -v n=$n -F'\n' '{
+f=$2
+l=$(wc -l < $f)
+echo "$n $l"
+cat $f | tr ' ' '\n' | tr -cd '[:alpha:] \n' | tr '[:upper:]' '[:lower:]' | awk -v n=$n -F'\n' '{
   for(i=1; i<=NF; i++) {
     for(i=1; i<=length($0)-n+1; i++) {
       print substr($0, i, n)
     }
   }
-}'
-done | sort | uniq -c | sort -nr
+}' | sort | uniq -c | sort -nr | sed 's/^[ \t]*//'
