972625731d32df779ccfeacffe0a86ea75f32d0f
Ben O'Neill <ben@oneill.sh>
2025-09-08 09:26:36 -0400

    Load ngrams from file generated by script

--- src/enigma/ngram.c
+++ src/enigma/ngram.c
@@ -1,8 +1,69 @@
 #include "ngram.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DEFAULT_NGRAM_COUNT 100000
+
 #define BIIDX(a, b) ((a << 5) | b)
 #define TRIIDX(a, b, c) ((a << 10) | (b << 5) | c)
 #define QUADIDX(a, b, c, d) ((a << 15) | (b << 10) | (c << 5) | d)
+
+typedef struct {
+    char ngram[5];
+    float score;
+    int count;
+} ngram_t;
+
+typedef struct {
+    ngram_t *ngrams;
+    int count;
+    int n;
+} ngram_list_t;
+
+ngram_list_t *enigma_init_ngrams(const char *path) {
+    char line[16];
+    int lineCount = 0;
+    int reallocCount = 0;
+    FILE *f = fopen(path, "r");
+    if (!f) {
+        fprintf(stderr, "Failed to open ngram file: %s\n", path);
+    }
+
+    ngram_list_t *ngram_list = malloc(sizeof(ngram_list_t));
+    ngram_list->ngrams = malloc(DEFAULT_NGRAM_COUNT * sizeof(ngram_t));
+    ngram_list->count = 0;
+    ngram_list->n = -1;
+
+    if (fgets(line, sizeof(line), f)) {
+        // First line should be n value and line count of original text
+        if (sscanf(line, "%d %d", &ngram_list->n, &lineCount) != 2) {
+            fprintf(stderr, "Invalid ngram file format: %s\n", path);
+            free(ngram_list->ngrams);
+            free(ngram_list);
+            fclose(f);
+            return NULL;
+        }
+    } else {
+        fprintf(stderr, "Failed to read ngram file: %s\n", path);
+        free(ngram_list->ngrams);
+        free(ngram_list);
+        fclose(f);
+        return NULL;
+    }
+
+    while (fgets(line, sizeof(line), f)) {
+        if (ngram_list->count >= DEFAULT_NGRAM_COUNT * (reallocCount + 1)) {
+            ngram_list->ngrams = realloc(ngram_list->ngrams, (ngram_list->count + DEFAULT_NGRAM_COUNT * ++reallocCount) * sizeof(ngram_t));
+        }
+        if (sscanf(line, "%d %4s", &ngram_list->ngrams[ngram_list->count].count, ngram_list->ngrams[ngram_list->count].ngram) == 2) {
+            ngram_list->count++;
+        }
+    }
+
+    fclose(f);
+    return ngram_list;
+}
 
 float enigma_bigram_score(const char *text, int textLen, const float *bigrams) {
     float total = 0.0f;
