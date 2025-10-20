a487ea7d46c6f4f9a0dc2fd324e2dc91bda3dab0
Ben O'Neill <ben@oneill.sh>
2025-10-20 11:27:28 -0400

    Update n-gram generator to use character count of original source

--- tools/ngram.sh
+++ tools/ngram.sh
@@ -2,11 +2,11 @@
 # ngram generator
 # usage: ngram.sh n file
 # outputs ngram counts to stdout.
-# first line of output is "n linecount"
+# first line of output is "n charcount"
 
 n=$1
 f=$2
-l=$(wc -l < $f)
+l=$(wc -c < $f)
 echo "$n $l"
 cat $f | tr ' ' '\n' | tr -cd '[:alpha:] \n' | tr '[:upper:]' '[:lower:]' | awk -v n=$n -F'\n' '{
   for(i=1; i<=NF; i++) {
