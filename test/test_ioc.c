62ae53238927d65e3bc97e180cbf33f7788e013a
Ben O'Neill <ben@oneill.sh>
2025-10-20 16:54:56 -0400

    test_ioc.c: Define ciphertextLen

--- test/test_ioc.c
+++ test/test_ioc.c
@@ -9,6 +9,7 @@ void tearDown(void) { }
 void test_enigma_ioc_score(void) {
     enigma_crack_config_t cfg;
     const char* text = "HELLOWORLD";
+    cfg.ciphertextLen = strlen(text);
 
     float score = enigma_ioc_score(&cfg, text);
     TEST_ASSERT_GREATER_THAN_FLOAT(0.0f, score);
