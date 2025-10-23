d0a326fe112209796ee8ef0fcf6bf6080db7862e
Ben O'Neill <ben@oneill.sh>
2025-10-23 15:31:04 -0400

    More test work

--- test/test_crack.c
+++ test/test_crack.c
@@ -3,9 +3,12 @@
 #include "enigma/crack.c"
 #include "enigma/enigma.h"
 
+#include <time.h>
+
 enigma_crack_config_t cfg;
 
 void setUp(void) {
+    srand(time(NULL));
     memset(&cfg, 0, sizeof(enigma_crack_config_t));
 }
 
@@ -241,13 +244,35 @@ void test_enigma_score_flags_WithKnownPlaintextFlag_WithNullPlaintext(void) {
 }
 
 void test_enigma_score_sort(void) {
-    // TODO Implement
+    enigma_score_list_t scores;
+    scores.maxScores = 10;
+    scores.scores = malloc(sizeof(enigma_score_t) * scores.maxScores);
+    scores.scoreCount = 10;
+    for (int i = 0; i < scores.scoreCount; i++) {
+        scores.scores[i].score = ((float) rand()) / ((float)rand());
+    }
+
+    int ret = enigma_score_sort(&scores);
+
+    TEST_ASSERT_EQUAL_INT(0, ret);
+    for (int i = 1; i < scores.scoreCount; i++) {
+        TEST_ASSERT_TRUE(scores.scores[i - 1].score >= scores.scores[i].score);
+    }
+
+    free(scores.scores);
 }
 
 void test_enigma_score_sort_WithNullArguments(void) {
-    // TODO Implement
+    TEST_ASSERT_EQUAL_INT(1, enigma_score_sort(NULL));
 }
 
 void test_score_compare(void) {
-    // TODO Implement
+    enigma_score_t score1;
+    enigma_score_t score2;
+    score1.score = 0.5;
+    score2.score = 0.6;
+
+    TEST_ASSERT_EQUAL_INT(1, score_compare(&score1, &score2));
+    TEST_ASSERT_EQUAL_INT(-1, score_compare(&score2, &score1));
+    TEST_ASSERT_EQUAL_INT(0, score_compare(&score1, &score1));
 }
