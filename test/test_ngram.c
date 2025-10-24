396db44e819d785f884a989a3eeee516087db4db
Ben O'Neill <ben@oneill.sh>
2025-10-23 21:26:56 -0400

    N-gram tests

--- test/test_ngram.c
+++ test/test_ngram.c
@@ -1,37 +1,66 @@
+#include "enigma/crack.h"
 #include "unity.h"
 #include "enigma/ngram.h"
 
+#include <stdlib.h>
 #include <string.h>
 
+#define I(s) (s - 'A')
+
+enigma_crack_config_t cfg;
+const char* plaintext = "EVERXTRIEDXEVERXFAILEDXNOXMATTERXTRYXAGAINXFAILXAGAINXFAILXBETTER";
+
+void setUp(void) {
+    memset(&cfg, 0, sizeof(enigma_crack_config_t));
+    cfg.ciphertextLen = strlen(plaintext);
+}
+
+void loadNgrams(int n) {
+    int len = strlen(plaintext);
+    for (int i = 0; i < len - 7; i++) {
+        switch (n) {
+            case 2:
+                cfg.ngrams[ENIGMA_BIIDX(I(plaintext[i]), I(plaintext[i+1]))] += 1.0 / len; break;
+            case 3:
+                cfg.ngrams[ENIGMA_TRIIDX(I(plaintext[i]), I(plaintext[i+1]), I(plaintext[i+2]))] += 1.0 / len; break;
+            case 4:
+                cfg.ngrams[ENIGMA_QUADIDX(I(plaintext[i]), I(plaintext[i+1]), I(plaintext[i+2]), I(plaintext[i+3]))] += 1.0 / len; break;
+        }
+    }
+
+}
+
 void test_enigma_bigram_score(void) {
-    enigma_crack_config_t cfg;
-    const char* text = "ABCD";
-    cfg.ciphertextLen = strlen(text);
+    cfg.n = 2;
+    cfg.ngrams = calloc((26<<5)|26, sizeof(float));
+    loadNgrams(2);
 
-    // TODO Prepare n-grams
+    float score = enigma_bigram_score(&cfg, plaintext);
+    TEST_ASSERT_GREATER_THAN_FLOAT(0.0f, score);
 
-    //float score = enigma_bigram_score(&cfg, text);
-    //TEST_ASSERT_GREATER_THAN_FLOAT(0.0f, score);
+    free(cfg.ngrams);
 }
 
 void test_enigma_trigram_score(void) {
-    enigma_crack_config_t cfg;
-    const char* text = "HELLOWORLD";
-    cfg.ciphertextLen = strlen(text);
+    cfg.ciphertextLen = strlen(plaintext);
+    cfg.n = 3;
+    cfg.ngrams = calloc((26<<10)|(26<<5)|26, sizeof(float));
+    loadNgrams(3);
 
-    // TODO Prepare n-grams
+    float score = enigma_trigram_score(&cfg, plaintext);
+    TEST_ASSERT_GREATER_THAN_FLOAT(0.0f, score);
 
-    //float score = enigma_trigram_score(&cfg, text);
-    //TEST_ASSERT_GREATER_THAN_FLOAT(0.0f, score);
+    free(cfg.ngrams);
 }
 
 void test_enigma_quadgram_score(void) {
-    enigma_crack_config_t cfg;
-    const char* text = "HELLOWORLD";
-    cfg.ciphertextLen = strlen(text);
+    cfg.ciphertextLen = strlen(plaintext);
+    cfg.n = 4;
+    cfg.ngrams = calloc((26<<15)|(26<<10)|(26<<5)|26, sizeof(float));
+    loadNgrams(4);
 
-    // TODO Prepare n-grams
+    float score = enigma_quadgram_score(&cfg, plaintext);
+    TEST_ASSERT_GREATER_THAN_FLOAT(0.0f, score);
 
-    //float score = enigma_quadgram_score(&cfg, text);
-    //TEST_ASSERT_GREATER_THAN_FLOAT(0.0f, score);
+    free(cfg.ngrams);
 }
