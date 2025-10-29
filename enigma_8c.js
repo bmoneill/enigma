7216b90168f1a42e58b8bfae69f7848214e851ef
bmoneill <benoneill617@gmail.com>
2025-10-29 21:13:53 +0000

    Deploying to gh-pages from @ bmoneill/enigma@c08bf9d9c862b996355b71dd56dba9400d2da842 🚀

--- enigma_8c.js
+++ enigma_8c.js
@@ -5,5 +5,10 @@ var enigma_8c =
     [ "enigma_encode_string", "enigma_8c.html#a2eb0c5feefd66fffa1c67b40d6936e2a", null ],
     [ "enigma_init_default_config", "enigma_8c.html#aa4f7392a67f22f7cf6d541a0830376f7", null ],
     [ "enigma_init_random_config", "enigma_8c.html#a0f268fe185f30eedc4d3bdfce22cdb99", null ],
-    [ "enigma_init_rotors", "enigma_8c.html#afa6ddeea3814ebe1d80d173520fe02e9", null ]
+    [ "enigma_init_rotors", "enigma_8c.html#afa6ddeea3814ebe1d80d173520fe02e9", null ],
+    [ "enigma_set_plugboard", "enigma_8c.html#af0e33a2795dfa1a5cb35f21c6429c5a9", null ],
+    [ "enigma_set_reflector", "enigma_8c.html#ad7bb3ee7b4001b692893cccd7b819a92", null ],
+    [ "enigma_set_rotor", "enigma_8c.html#a1d26670a1b42dac634fb1fa40ba001c0", null ],
+    [ "enigma_set_rotor_count", "enigma_8c.html#a47c4b57fc250834e81c8b0dc91c1d168", null ],
+    [ "enigma_set_rotor_index", "enigma_8c.html#ac9d805aeb8cddc00388ed51f5e2a3e8d", null ]
 ];
--- enigma_8h.js
+++ enigma_8h.js
@@ -5,5 +5,10 @@ var enigma_8h =
     [ "enigma_encode_string", "enigma_8h.html#a7028f7fe850510bf94652bfb49a7e119", null ],
     [ "enigma_init_default_config", "enigma_8h.html#a6160e12e9ba3a8f2253f8932b8d88372", null ],
     [ "enigma_init_random_config", "enigma_8h.html#a2d84c997593fbedc7f512106fc36e07a", null ],
-    [ "enigma_init_rotors", "enigma_8h.html#aed3654d1d34c50b21a2215a01f0dce25", null ]
+    [ "enigma_init_rotors", "enigma_8h.html#aed3654d1d34c50b21a2215a01f0dce25", null ],
+    [ "enigma_set_plugboard", "enigma_8h.html#af75c08bcf8c2fd34828b6156c6a745e2", null ],
+    [ "enigma_set_reflector", "enigma_8h.html#a6511f1dcf4e3e8a4d7ba82cb4afdb3cb", null ],
+    [ "enigma_set_rotor", "enigma_8h.html#a25fc2287975a95eb0139fdcbe35fd077", null ],
+    [ "enigma_set_rotor_count", "enigma_8h.html#accc2aa716eb00626ed942d7e72aade20", null ],
+    [ "enigma_set_rotor_index", "enigma_8h.html#ab21c8ec8bf4b32bc6fb37a7666c9ef7d", null ]
 ];
