2c7a64e5b99b0908c5545ce2aba90f25daa214c2
Ben O'Neill <ben@oneill.sh>
2025-10-29 10:06:59 -0400

    Add rotor index table generation script

---
+++ tools/indexgen.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# Generate index tables for a given rotor alphabet
+
+import sys
+
+
+def toi(c):
+    return ord(c.upper()) - ord("A")
+
+
+def generate_indexes(rotalpha):
+    if len(rotalpha) != 26:
+        raise ValueError("Rotor alphabet must be 26 characters long")
+
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    fwd = []
+    rev = [-1] * 26
+    for i in range(len(alpha)):
+        fwdidx = toi(rotalpha[i])
+        fwd.append(fwdidx)
+
+    for c in alpha:
+        for i in range(len(rotalpha)):
+            if rotalpha[i] == c:
+                rev[toi(c)] = i
+
+    return fwd, rev
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: %s <rotor_alphabet>" % sys.argv[0])
+        return
+    fwd, rev = generate_indexes(sys.argv[1])
+    print(fwd)
+    print(rev)
+
+
+if __name__ == "__main__":
+    main()
