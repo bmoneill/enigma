082f1de2c1f572d85f06171dabdfc9d94624fa15
bmoneill <benoneill617@gmail.com>
2025-11-02 01:14:39 +0000

    Deploying to gh-pages from @ bmoneill/enigma@c59024a1103066fdd672bd9ee369652b2a6c654a 🚀

--- reflector_8h.js
+++ reflector_8h.js
@@ -3,8 +3,8 @@ var reflector_8h =
     [ "enigma_reflector_t", "structenigma__reflector__t.html", "structenigma__reflector__t" ],
     [ "ENIGMA_REFLECTOR_COUNT", "reflector_8h.html#a076c516101d818406c4cf369d70095c8", null ],
     [ "enigma_reflector_generate_indices", "reflector_8h.html#a50202bc66091342d81973e7de692f93b", null ],
-    [ "enigma_reflector_get_indices", "reflector_8h.html#a74486ef75c97e4183ab21b2d9e04587d", null ],
-    [ "enigma_reflector_get_name", "reflector_8h.html#a6520abadf48c2c5f8f8fcad634d69744", null ],
-    [ "enigma_reflector_set_indices", "reflector_8h.html#a59b75cca300012fb848dc777935d01b3", null ],
-    [ "enigma_reflector_set_name", "reflector_8h.html#a34b09b143060e83c43be5addc9b94f49", null ]
+    [ "enigma_reflector_get_indices", "reflector_8h.html#a253f1e0aa88fda4ec134c640893b56ba", null ],
+    [ "enigma_reflector_get_name", "reflector_8h.html#ae155dfc5106669f45754a8559d322c9f", null ],
+    [ "enigma_reflector_set_indices", "reflector_8h.html#a3e94840bdcaafd26da226ceb673d74f1", null ],
+    [ "enigma_reflector_set_name", "reflector_8h.html#a3648114499b2c7bdd4a3e5c3df99e87c", null ]
 ];
