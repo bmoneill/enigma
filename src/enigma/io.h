5e5095d47c1fedb62e4466fdfa0df5ea5470b465
Ben O'Neill <ben@oneill.sh>
2025-11-01 20:04:35 -0400

    Formatting

--- src/enigma/io.h
+++ src/enigma/io.h
@@ -13,15 +13,15 @@
 
 static const char* enigma_invalid_argument_message = "Invalid argument provided.";
 
-int  enigma_error_message(const char*, const char*, ...);
-int  enigma_load_config(enigma_t*, const char*);
-int  enigma_load_custom_reflector(enigma_reflector_t*, const char*, const char*);
-int  enigma_load_custom_rotor(enigma_rotor_t*, const char*, const char*, int*, int);
-int  enigma_load_ngrams(enigma_crack_t*, const char*);
-int  enigma_load_plugboard_config(enigma_t*, const char*);
-int  enigma_load_reflector_config(enigma_t*, const char*);
-int  enigma_load_rotor_config(enigma_t*, char*);
-int  enigma_load_rotor_positions(enigma_t*, const char*);
-void enigma_print_config(const enigma_t*, char*);
+int                enigma_error_message(const char*, const char*, ...);
+int                enigma_load_config(enigma_t*, const char*);
+int                enigma_load_custom_reflector(enigma_reflector_t*, const char*, const char*);
+int                enigma_load_custom_rotor(enigma_rotor_t*, const char*, const char*, int*, int);
+int                enigma_load_ngrams(enigma_crack_t*, const char*);
+int                enigma_load_plugboard_config(enigma_t*, const char*);
+int                enigma_load_reflector_config(enigma_t*, const char*);
+int                enigma_load_rotor_config(enigma_t*, char*);
+int                enigma_load_rotor_positions(enigma_t*, const char*);
+void               enigma_print_config(const enigma_t*, char*);
 
 #endif
