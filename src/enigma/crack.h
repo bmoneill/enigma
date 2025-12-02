681a2d7d1ebe2ec2476d89134d16f82b137d3068
Ben O'Neill <ben@oneill.sh>
2025-12-01 22:16:43 -0500

    Remove unused code

--- src/enigma/crack.h
+++ src/enigma/crack.h
@@ -55,17 +55,6 @@ typedef struct {
     int                  known_plaintext_length;
 } enigma_crack_t;
 
-/**
- * English letter frequency targets (from
- * https://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html).
- *
- * X is probably unreliable here due to possibly being used as a space substitute.
- */
-static const float englishFreq[26]
-    = { 0.0812f, 0.0149f, 0.0271f, 0.0432f, 0.1202f, 0.023f,  0.0203f, 0.0592f, 0.0731f,
-        0.0010f, 0.0069f, 0.0398f, 0.0261f, 0.0695f, 0.0768f, 0.0182f, 0.0011f, 0.0602f,
-        0.0628f, 0.0910f, 0.0288f, 0.0111f, 0.0209f, 0.0017f, 0.0211f, 0.0007f };
-
 int   enigma_crack_rotor(enigma_crack_t*, int, float (*)(const enigma_crack_t*, const char*));
 int   enigma_crack_rotors(enigma_crack_t*, float (*)(const enigma_crack_t*, const char*));
 int   enigma_crack_rotor_positions(enigma_crack_t*, float (*)(const enigma_crack_t*, const char*));
