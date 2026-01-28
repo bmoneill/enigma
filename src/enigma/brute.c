265d8a822951efdedcf7debc069d7a151dd1c5b6
Ben O'Neill <ben@oneill.sh>
2026-01-28 10:42:56 -0500

    enigma_brute_score: Add docstring

--- src/enigma/brute.c
+++ src/enigma/brute.c
@@ -8,6 +8,14 @@
 
 #include "common.h"
 
+/**
+ * @brief Calculate the score of a given text using the brute force dictionary cracking method.
+ *
+ * @param cfg The configuration parameters for the Enigma machine.
+ * @param text The text to be scored.
+ * @return The score of the text. It will be 1.0 if two dictionary words exist in the plaintext,
+ *         and 0.0 otherwise.
+ */
 EMSCRIPTEN_KEEPALIVE float enigma_brute_score(const EnigmaCrackParams* cfg, const char* text) {
     return (float) enigma_dict_match(cfg, text);
 }
