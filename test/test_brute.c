39f92db2fffc3095ce3dc2bdfd0455fc31149ca0
Ben O'Neill <ben@oneill.sh>
2026-04-27 14:53:12 -0400

    Fix test_brute.c

--- test/test_brute.c
+++ test/test_brute.c
@@ -1,4 +1,5 @@
 #include "enigma/brute.h"
+#include "enigma/io.h"
 #include "unity.h"
 
 #include <stdlib.h>
@@ -18,58 +19,36 @@ void tearDown(void) {}
 
 void test_enigma_brute_score_WithMatchingPlaintext_Not_X_Separated(void) {
     const char* plaintext = "HELLOXWORLDXFOOXBAR";
-    cfg.ciphertext_length = strlen(plaintext);
-    cfg.dictionary        = malloc(7 * sizeof(char*));
-    cfg.dictionary_length = 7;
+    const char* dictStr   = "BAR\nBAZ\nFOO\nGOODBYE\nHELLO\nTEST\nWORLD";
 
-    cfg.dictionary[0]     = "BAR";
-    cfg.dictionary[1]     = "BAZ";
-    cfg.dictionary[2]     = "FOO";
-    cfg.dictionary[3]     = "GOODBYE";
-    cfg.dictionary[4]     = "HELLO";
-    cfg.dictionary[5]     = "TEST";
-    cfg.dictionary[6]     = "WORLD";
+    int         ret       = enigma_load_dict_s(&cfg, dictStr, strlen(dictStr));
+    TEST_ASSERT_EQUAL_INT_MESSAGE(ENIGMA_SUCCESS, ret, "Expected enigma_load_dict_s to succeed");
 
-    TEST_ASSERT_EQUAL_FLOAT_MESSAGE(1.0,
-                                    enigma_brute_score(&cfg, plaintext),
-                                    "Expected dictionary to match");
+    ret = enigma_brute_score(&cfg, plaintext);
+    TEST_ASSERT_EQUAL_FLOAT_MESSAGE(1.0, ret, "Expected dictionary to match");
 
     free(cfg.dictionary);
 }
 
 void test_enigma_brute_score_WithMatchingPlaintext_X_Separated(void) {
     const char* plaintext = "HELLOXWORLDXFOOXBAR";
-    cfg.ciphertext_length = strlen(plaintext);
-    cfg.dictionary        = malloc(7 * sizeof(char*));
-    cfg.dictionary_length = 7;
+    const char* dictStr   = "BAR\nBAZ\nFOO\nGOODBYE\nHELLO\nTEST\nWORLD";
+    int         ret       = enigma_load_dict_s(&cfg, dictStr, strlen(dictStr));
+    TEST_ASSERT_EQUAL_INT_MESSAGE(ENIGMA_SUCCESS, ret, "Expected enigma_load_dict_s to succeed");
     cfg.flags |= ENIGMA_FLAG_X_SEPARATED;
 
-    cfg.dictionary[0] = "BAR";
-    cfg.dictionary[1] = "BAZ";
-    cfg.dictionary[2] = "FOO";
-    cfg.dictionary[3] = "GOODBYE";
-    cfg.dictionary[4] = "HELLO";
-    cfg.dictionary[5] = "TEST";
-    cfg.dictionary[6] = "WORLD";
-
-    TEST_ASSERT_EQUAL_FLOAT_MESSAGE(1.0,
-                                    enigma_brute_score(&cfg, plaintext),
-                                    "Expected dictionary to match");
+    ret = enigma_brute_score(&cfg, plaintext);
+    TEST_ASSERT_EQUAL_FLOAT_MESSAGE(1.0, ret, "Expected dictionary to match");
     free(cfg.dictionary);
 }
 
 void test_enigma_brute_score_WithNonMatchingPlaintext_X_Separated(void) {
-    cfg.dictionary        = malloc(7 * sizeof(char*));
-    cfg.dictionary_length = 7;
+    const char* dictStr   = "BAR\nBAZ\nEARTH\nFOO\nMARS\nSATURN\nTEST";
     cfg.ciphertext_length = strlen(helloWorld);
     cfg.flags |= ENIGMA_FLAG_X_SEPARATED;
-    cfg.dictionary[0] = "BAR";
-    cfg.dictionary[1] = "BAZ";
-    cfg.dictionary[2] = "EARTH";
-    cfg.dictionary[3] = "FOO";
-    cfg.dictionary[4] = "MARS";
-    cfg.dictionary[5] = "SATURN";
-    cfg.dictionary[6] = "TEST";
+
+    int ret = enigma_load_dict_s(&cfg, dictStr, strlen(dictStr));
+    TEST_ASSERT_EQUAL_INT_MESSAGE(ENIGMA_SUCCESS, ret, "Expected enigma_load_dict_s to succeed");
 
     TEST_ASSERT_EQUAL_FLOAT_MESSAGE(0.0,
                                     enigma_brute_score(&cfg, helloWorld),
@@ -78,13 +57,11 @@ void test_enigma_brute_score_WithNonMatchingPlaintext_X_Separated(void) {
 }
 
 void test_enigma_brute_score_WithNonMatchingPlaintext_Not_X_Separated(void) {
-    cfg.dictionary        = malloc(4 * sizeof(char*));
+    const char* dictStr   = "EARTH\nFOO\nMARS\nSATURN";
     cfg.ciphertext_length = strlen(helloWorld);
-    cfg.dictionary_length = 4;
-    cfg.dictionary[0]     = "EARTH";
-    cfg.dictionary[1]     = "FOO";
-    cfg.dictionary[2]     = "MARS";
-    cfg.dictionary[3]     = "SATURN";
+
+    int ret               = enigma_load_dict_s(&cfg, dictStr, strlen(dictStr));
+    TEST_ASSERT_EQUAL_INT_MESSAGE(ENIGMA_SUCCESS, ret, "Expected enigma_load_dict_s to succeed");
 
     TEST_ASSERT_EQUAL_FLOAT_MESSAGE(0.0,
                                     enigma_brute_score(&cfg, helloWorld),
